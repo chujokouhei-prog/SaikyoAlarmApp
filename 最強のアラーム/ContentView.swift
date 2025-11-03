978ea54270a8637d5293a88568fea892b3db44d5
chuujokohei <chuujokohei@yahoo.co.jp>
2025-11-03 19:15:27 +0900

    Initial Commit

---
+++ 最強のアラーム/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  最強のアラーム
+//
+//  Created by 中條航平 on 2025/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 最強のアラーム/_______App.swift
@@ -0,0 +1,17 @@
+//
+//  _______App.swift
+//  最強のアラーム
+//
+//  Created by 中條航平 on 2025/11/03.
+//
+
+import SwiftUI
+
+@main
+struct _______App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
